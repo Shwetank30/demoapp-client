2dabcd0d1b1e0c332df6d570acd044ef12f90692
Shwetank30 <shwetank.kumar.cer13@itbhu.ac.in>
2018-03-18 13:48:16 +0530

    Fixed auto-refresh of JWT tokens

--- src/apollo.js
+++ src/apollo.js
@@ -16,24 +16,26 @@ const middlewareLink = setContext(() => ({
 }));
 
 const afterwareLink = new ApolloLink((operation, forward) => {
-  const { headers } = operation.getContext();
+  return forward(operation).map((response) => {
+    const { response: { headers } } = operation.getContext();
+    if (headers) {
+      const token = headers.get('x-token');
+      const refreshToken = headers.get('x-refresh-token');
 
-  if (headers) {
-    const token = headers.get('x-token');
-    const refreshToken = headers.get('x-refresh-token');
-
-    if (token) {
-      localStorage.setItem('token', token);
+      if (token) {
+        localStorage.setItem('token', token);
+      }
+      if (refreshToken) {
+        localStorage.setItem('refreshToken', refreshToken);
+      }
     }
-    if (refreshToken) {
-      localStorage.setItem('refreshToken', refreshToken);
-    }
-  }
-
-  return forward(operation);
+    return response;
+  });
 });
 
-const httpLinkWithMiddleWare = afterwareLink.concat(middlewareLink.concat(httpLink));
+const httpLinkWithMiddleWare = afterwareLink.concat(
+  middlewareLink.concat(httpLink),
+);
 
 const wsLink = new WebSocketLink({
   uri: 'ws://localhost:8081/subscriptions',
